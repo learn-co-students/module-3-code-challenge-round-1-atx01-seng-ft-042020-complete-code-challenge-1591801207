d3bd8805d1d9b187d88809c324fce30e20d1e0ed
agomezdemen <agomezdemen15@gmail.com>
2020-06-10 12:02:47 -0500

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 5413 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -10,3 +10,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
 })
+
+let likes = 0
+const likeBut = document.getElementById("like_button")
+imageData = fetch('https://randopic.herokuapp.com/images/5413')
+let myImg = document.getElementById("image")
+let commentForm = document.getElementById("comment_form")
+
+myImg = imageData
+
+likeBut.addEventListener('click', event => {
+  likes++
+  document.getElementById("likes").innerHTML = `${likes}`
+})
+
+commentForm.addEventListener('submit', event => {
+
+})
+
+//i was stuck trying to access the properties inside the image data and didnt realize i lost so much time
